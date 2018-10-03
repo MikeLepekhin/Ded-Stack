323f417b8d2cb757ea9c696e19cbf866a5f729ea
MikeLepekhin <supersky2325@gmail.com>
2018-10-03 20:25:54 +0300

    dump function, canaries and hash sum validation were added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ void printFirstK(int k) {
   Stack<int> st;
   for (int i = 0; i < k; ++i) {
     st.push(i);
+    if (i % 10 == 3) {
+      st.dump();
+    }
   }
   for (int i = 0; i < k; ++i) {
     std::cout << st[i] << ' ';
@@ -16,11 +19,22 @@ void printFirstK(int k) {
   }
 }
 
+void copyStack() {
+  Stack<std::string> st;
+  st.push("hello, comrade!");
+  st.push("stack overflow");
+  std::cout << st[0] << ' ' << st[1] << '\n';
+  Stack<std::string> st2 = st;
+  std::cout << st2[0] << ' ' << st2[1] << '\n';
+}
+
 int main() {
   std::cout << "Hello, user of my stack!" << std::endl;
 
   try {
+    //copyStack();
     printFirstK(1000);
+    printFirstK(83);
   } catch (StackException stackException) {
     std::cerr << stackException << '\n';
   }
--- my_stack.h
+++ my_stack.h
@@ -6,6 +6,7 @@
 #define STACK_MY_STACK_H
 
 #include <cstdint>
+#include <utility>
 
 #include "exception.h"
 
@@ -54,20 +55,26 @@ class Stack {
     return reinterpret_cast<T*>(items_begin_ + pos * sizeof(T));
   }
 
-  void setCapacity(size_t new_capacity) {
-    char* new_bytes = new char[new_capacity * sizeof(T) + 2 * CANARY_SIZE + 1];
-    T* cur_item = reinterpret_cast<T*>(items_begin_);
-    T* new_item = reinterpret_cast<T*>(new_bytes + CANARY_SIZE);
+  void copyItems(const char* from, char* to) {
+    const T* cur_item = reinterpret_cast<const T*>(from + CANARY_SIZE);
+    T* new_item = reinterpret_cast<T*>(to + CANARY_SIZE);
 
     for (size_t item_id = 0; item_id < item_count_; ++item_id, ++cur_item, ++new_item) {
       *new_item = *cur_item;
     }
+  }
 
+  void setCapacity(size_t new_capacity) {
+    char* new_bytes = new char[new_capacity * sizeof(T) + 2 * CANARY_SIZE + 1];
+
+    copyItems(bytes_, new_bytes);
+    destroy();
     std::swap(new_bytes, bytes_);
     items_begin_ = bytes_ + CANARY_SIZE;
     capacity_ = new_capacity;
+#ifndef NDEBUG
     initCanaries();
-    delete[] new_bytes;
+#endif
   }
 
   void extend() {
@@ -78,12 +85,151 @@ class Stack {
     setCapacity(capacity_ / 2);
   }
 
+  void destroy() {
+    for (size_t item_id = 0; item_id < item_count_; ++item_id) {
+      getElementPtr(item_id)->~T();
+    }
+    delete[] bytes_;
+  }
+
+  template <class StackRef>
+  void copyParameters(const StackRef&& another) {
+    capacity_ = another.capacity_;
+    item_count_ = another.item_count_;
+    items_begin_ = another.items_begin_;
+#ifndef NDEBUG
+    initCanaries();
+    hash_sum_ = another.hash_sum_;
+    hasher_ = another.hasher_;
+#endif
+  }
+
+#ifndef NDEBUG
+  bool checkCanaries() const {
+    bool all_fine = true;
+
+    std::cerr << "canary 1 = " << ' ' << *getCanaryPtr1() << '\n';
+    if (*getCanaryPtr1() == CANARY_INIT_VALUE) {
+      std::cerr << "ok\n";
+    } else {
+      std::cerr << "incorrect value! should be " << CANARY_INIT_VALUE << '\n';
+      all_fine = false;
+    }
+    std::cerr << "canary 2 = " << ' ' << *getCanaryPtr2() << '\n';
+    if (*getCanaryPtr2() == CANARY_INIT_VALUE) {
+      std::cerr << "ok\n";
+    } else {
+      std::cerr << "incorrect value! should be " << CANARY_INIT_VALUE << '\n';
+      all_fine = false;
+    }
+    return all_fine;
+  }
+
+  bool checkHashSum() const {
+    std::cerr << "saved hash sum: " << hash_sum_ << '\n';
+    size_t correct_hash_sum = calcHashSum();
+    if (hash_sum_ == correct_hash_sum) {
+      std::cerr << "ok\n";
+      return true;
+    } else {
+      std::cerr << "incorrect value! should be " << correct_hash_sum << '\n';
+      return false;
+    }
+  }
+
+  void printItems() const {
+    std::cerr << "stack_begin_address: " << &items_begin_ << '\n';
+    std::cerr << "item count: " << item_count_ << '\n';
+    std::cerr << "items: ";
+    for (size_t item_id = 0; item_id < item_count_; ++item_id) {
+      std::cerr << *getElementPtr(item_id) << ' ';
+    }
+    std::cerr << '\n';
+  }
+
+  const size_t MODULO = 1e12 + 7;
+  const size_t BASE = 15487469;
+
+  size_t calcHashSum() const {
+    size_t res = 0;
+
+    for (size_t item_id = 0; item_id < item_count_; ++item_id) {
+      res = (res * BASE + hasher_(*getElementPtr(item_id))) % MODULO;
+    }
+    return res;
+  }
+
+  void assertCanaries(const std::string& func_name = "") const {
+    if (*getCanaryPtr1() != CANARY_INIT_VALUE) {
+      throw CanaryException("problem with first canary", func_name);
+    }
+    if (*getCanaryPtr2() != CANARY_INIT_VALUE) {
+      throw CanaryException("problem with second canary", func_name);
+    }
+  }
+
+  void assertHashSum(const std::string& func_name = "") const {
+    if (hash_sum_ != calcHashSum()) {
+      throw HashSumException("", func_name);
+    }
+  }
+
+  void assertCorrectness() const {
+    assertCanaries();
+    assertHashSum();
+  }
+#endif
+
  public:
   Stack() {
     bytes_ = new char[MIN_CAPACITY * sizeof(T) + 2 * CANARY_SIZE + 1];
     items_begin_ = bytes_ + CANARY_SIZE;
     capacity_ = MIN_CAPACITY;
+#ifndef NDEBUG
     initCanaries();
+#endif
+    std::cerr << "stack was created\n";
+  }
+
+  Stack(Stack&& another) {
+    bytes_ = another.bytes_;
+    copyParameters(another);
+    another.bytes_ = nullptr;
+  }
+
+  Stack(const Stack& another) {
+    bytes_ = another.bytes_;
+    copyParameters(std::forward<const Stack>(another));
+    bytes_ = new char[2 * CANARY_SIZE + item_count_ * sizeof(T) + 1];
+    copyItems(another.bytes_, bytes_);
+  }
+
+  T& operator=(Stack&& another) {
+    if (this == &another) {
+      return *this;
+    }
+#ifndef NDEBUG
+    assertCorrectness();
+#endif
+    destroy();
+    bytes_ = another.bytes_;
+    copyParameters(another);
+    another.bytes_ = nullptr;
+    return *this;
+  }
+
+  T& operator=(const Stack& another) {
+    if (this == &another) {
+      return *this;
+    }
+#ifndef NDEBUG
+    assertCorrectness();
+#endif
+    destroy();
+    copyParameters(another);
+    bytes_ = new char[2 * CANARY_SIZE + item_count_ * sizeof(T) + 1];
+    copyItems(another.bytes_, bytes_);
+    return *this;
   }
 
   T& operator[](size_t pos) {
@@ -105,8 +251,7 @@ class Stack {
       extend();
     }
 #ifndef NDEBUG
-    checkCanaries(__PRETTY_FUNCTION__);
-    checkHashSum(__PRETTY_FUNCTION__);
+    assertCorrectness();
 #endif
     *getElementPtr(item_count_) = value;
     ++item_count_;
@@ -123,8 +268,7 @@ class Stack {
       shrink();
     }
 #ifndef NDEBUG
-    checkCanaries(__PRETTY_FUNCTION__);
-    checkHashSum(__PRETTY_FUNCTION__);
+    assertCorrectness();
 #endif
     --item_count_;
 #ifndef NDEBUG
@@ -141,53 +285,40 @@ class Stack {
   }
 
 #ifndef NDEBUG
-  const size_t MODULO = 1e12 + 7;
-  const size_t BASE = 15487469;
-
-  size_t calcHashSum() const {
-    size_t res = 0;
-
-    for (size_t item_id = 0; item_id < item_count_; ++item_id) {
-      res = (res * BASE + hasher_(*getElementPtr(item_id))) % MODULO;
-    }
-    return res;
-  }
-
-  void checkCanaries(const std::string& func_name = "") {
-    if (*getCanaryPtr1() != CANARY_INIT_VALUE) {
-      throw CanaryException("problem with first canary", func_name);
-    }
-    if (*getCanaryPtr2() != CANARY_INIT_VALUE) {
-      throw CanaryException("problem with second canary", func_name);
-    }
-  }
-
-  void checkHashSum(const std::string& func_name = "") {
-    if (hash_sum_ != calcHashSum()) {
-      throw HashSumException("", func_name);
-    }
-  }
-
   bool check() const {
     try {
-      checkCanaries();
-      checkHashSum();
+      assertCanaries();
+      assertHashSum();
       return true;
     } catch (StackException) {
       return false;
     }
   }
 
-  void dump() {
-    std::cout << "azaza ahaha\n";
+  void dump() const {
+    std::cerr << "__________________________________________\n";
+    bool correct_canaries = checkCanaries();
+    printItems();
+    bool correct_hash_sum = checkHashSum();
+    bool total_correct = correct_canaries || correct_hash_sum;
+
+    if (total_correct) {
+      std::cerr << "All right\n";
+      return;
+    }
+
+    std::cerr << "A problem occured\n";
+    if (!correct_canaries) {
+      std::cerr << "canaries were overwritten\n";
+    }
+    if (!correct_hash_sum) {
+      std::cerr << "control hash sum don't match\n";
+    }
   }
 #endif
 
   ~Stack() {
-    for (size_t item_id = 0; item_id < item_count_; ++item_id) {
-      getElementPtr(item_id)->~T();
-    }
-    delete[] bytes_;
+    destroy();
   }
 };
 
